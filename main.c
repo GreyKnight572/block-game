62bdd2a64a38a408b44db9265431bac198430aeb
GreyKnight572 <104874257+GreyKnight572@users.noreply.github.com>
2023-10-26 16:01:13 -0400

    Added comments and window name

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
-#include <windows.h>
+#include <windows.h> // Win32 API
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM); // Window procedure
 
+// Entry point
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	PWSTR pCmdLine, int nCmdShow) {
 
-	MSG msg;
-	HWND hwnd;
-	WNDCLASSW wc;
+	WNDCLASSW wc; // Window class
+	HWND hwnd;	  // Window handle
+	MSG msg;	  // Current message
 
+	// Register window class
 	wc.style		 = CS_HREDRAW | CS_VREDRAW;
 	wc.cbClsExtra	 = 0;
 	wc.cbWndExtra	 = 0;
@@ -19,33 +21,37 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	wc.lpfnWndProc	 = WndProc;
 	wc.hCursor		 = LoadCursor(NULL, IDC_ARROW);
 	wc.hIcon		 = LoadIcon(NULL, IDI_APPLICATION);
-
 	RegisterClassW(&wc);
-	hwnd = CreateWindowW(wc.lpszClassName, L"Window",
-				WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-				100, 100, 350, 250, NULL, NULL, hInstance, NULL);
 
+	// Create the window
+	hwnd = CreateWindowW(wc.lpszClassName, L"Block Game",
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 350, 250,
+		NULL, NULL, hInstance, NULL);
 	ShowWindow(hwnd, nCmdShow);
 	UpdateWindow(hwnd);
 
+	// Message loop
 	while (GetMessage(&msg, NULL, 0, 0)) {
 
+		// Dispatch message to window procedure
 		DispatchMessage(&msg);
 	}
 
-	return (int) msg.wParam;
+	// Set application exit code
+	return msg.wParam;
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
-	WPARAM wParam, LPARAM lParam) {
+// Window procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 
 	switch(msg) {
 
-	  case WM_DESTROY:
-
-		  PostQuitMessage(0);
-		  break;
+		// Window closed
+		case WM_DESTROY:
+		PostQuitMessage(0); // Posts WM_QUIT with exit code
+		break;
 	}
 
+	// Process message with default window procedure
 	return DefWindowProcW(hwnd, msg, wParam, lParam);
 }

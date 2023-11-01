72674d5716525e2a656e7e9882b3cfde1aaabcc1
GreyKnight572 <104874257+GreyKnight572@users.noreply.github.com>
2023-11-01 19:38:56 -0400

    Back to basics, pt. 3

--- include/windows/window.h
+++
@@ -1,9 +0,0 @@
-#ifndef INCLUDE_GUARD_WINDOW_H
-#define INCLUDE_GUARD_WINDOW_H
-
-void SetMainWindow(char* windowTitle,
-	int windowWidth, int windowHeight, int showState);
-
-int MainWindowOpen(void);
-
-#endif
